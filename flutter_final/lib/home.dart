5be3d98ef6d3f4d1b5e77bc6fad73c8201177812
Benylaode <bayubulan659@gmail.com>
2024-06-20 11:40:02 +0800

    menyelesaikan tugas final flutter

---
+++ flutter_final/lib/home.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          title: const Text('Hai, Fahmi'),
+          actions: [
+            CircleAvatar(
+              child: FlutterLogo(size: 20),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 5, 0, 0),
+            child: Text(
+              "Foto  Favorite",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 500,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: List.generate(
+                  6,
+                  (index) => ScrollViewItem(
+                    width: 300,
+                    height: 400,
+                    imagePath:
+                        'images/${index + 1}.jpeg',
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScrollViewItem extends StatelessWidget {
+  final double width;
+  final double height;
+  final String imagePath;
+
+  const ScrollViewItem({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: width,
+      height: height,
+      margin: const EdgeInsets.only(right: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(size: 150),
+            SizedBox(height: 200),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(300, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
